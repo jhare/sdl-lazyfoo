7596319b45fb07cc1ea69f8bcc9febc1306a6ebf
Joe Hare <joseph.hare@gmail.com>
2018-01-10 07:52:11 -0600

    Have it be a black background

--- lesson1/my_hello.c
+++ lesson1/my_hello.c
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
     } else {
       screenSurface = SDL_GetWindowSurface(window);
 
-      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+      SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
 
       SDL_UpdateWindowSurface(window);
 
