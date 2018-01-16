943ce85420998be8c04f58c2e27c8b2dc4ed4ab8
Joe Hare <joseph.hare@gmail.com>
2018-01-15 19:13:47 -0600

    complete viewport lesson

--- lesson9-viewport/viewport.cpp
+++ lesson9-viewport/viewport.cpp
@@ -6,14 +6,13 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-SDL_Texture* loadTexture(std::string path);
-
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Texture* gTexture = NULL;
 
 bool init();
 bool loadMedia();
+SDL_Texture* loadTexture(std::string path);
 void reportError(std::string msg);
 void close();
 
@@ -28,6 +27,25 @@ int main(int argc, char* argv[])
       SDL_Event e;
       bool quit = false;
 
+      // Let's not define these in the event loop
+      SDL_Rect topLeftViewport;
+      topLeftViewport.x = 0;
+      topLeftViewport.y = 0;
+      topLeftViewport.w = SCREEN_WIDTH / 2;
+      topLeftViewport.h = SCREEN_HEIGHT / 2;
+
+      SDL_Rect topRightViewport;
+      topRightViewport.x = SCREEN_WIDTH / 2;
+      topRightViewport.y = 0;
+      topRightViewport.w = SCREEN_WIDTH / 2;
+      topRightViewport.h = SCREEN_HEIGHT / 2;
+
+      SDL_Rect bottomViewport;
+      bottomViewport.x = 0;
+      bottomViewport.y = SCREEN_HEIGHT / 2;
+      bottomViewport.w = SCREEN_WIDTH;
+      bottomViewport.h = SCREEN_HEIGHT / 2;
+
       while(!quit) {
         while(SDL_PollEvent(&e) != 0) {
           if(e.type == SDL_QUIT) {
@@ -37,6 +55,18 @@ int main(int argc, char* argv[])
 
         SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
         SDL_RenderClear(gRenderer);
+
+        // Each time we set the render viewport, then ask to render the texture
+        // with full source and destination rectangles (NULL, NULL)
+        SDL_RenderSetViewport(gRenderer, &topLeftViewport);
+        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+        SDL_RenderSetViewport(gRenderer, &topRightViewport);
+        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+        SDL_RenderSetViewport(gRenderer, &bottomViewport);
+        SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
         SDL_RenderPresent(gRenderer);
       }
     }
